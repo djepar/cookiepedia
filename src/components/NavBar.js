ceb37705c6be6ee8af641db2f010a128aea5b2cc
Dje Par <jparadis21@hotmail.com>
2022-10-14 18:19:11 +0800

    Add image and style to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,20 @@
 import Home from "./Home"
 import Recipes from "./Recipes";
 import {Routes, Route, Link } from 'react-router-dom';
-
+import cookie from '../assets/images/cookie-logo.png';
+import './style/navbar.css'
 function NavBar(){
     return(
         <div className="NavBar">
             <nav className="nav">
-                <Link to="/" className="nav-item">Home</Link>
-                <Link to="/recipes" className="nav-item">Recipes</Link>
-            </nav>
+                
+                <img src={cookie} alt="cookie icon"/>
+                <button><Link to="/" className="nav-item">Home</Link></button>
+                <button><Link to="/recipes" className="nav-item">Recipes</Link></button>
+                <div className="search">
+
+                </div>
+                </nav>
             <Routes>
                 <Route path="/" element={<Home/> } />
                 <Route path="/recipes" element={<Recipes/> } />
